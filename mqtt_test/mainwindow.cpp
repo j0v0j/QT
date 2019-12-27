45d379a9c92f9cabc4f724b1f77255e59dc2ec9e
WCH <wangchunhui1518@163.com>
2019-12-27 08:18:44 +0800

    aliyunmqtt

---
+++ mqtt_test/mainwindow.cpp
@@ -0,0 +1,72 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageAuthenticationCode>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    client->setClientId(clientId);
+    connect(client,&QMQTT::Client::subscribed,this,&MainWindow::somethingmqsub);
+    client->setUsername(DeviceName + "&" + ProductKey);
+//    client->setPassword(QMessageAuthenticationCode::hash(message.toLocal8Bit(), DeviceSecret.toLocal8Bit(), QCryptographicHash::Sha1).toHex());
+
+    client->setPassword("69137A72ED58BE7C41B1D4FB70169E5C32711AB9");
+    connect(client,&QMQTT::Client::connected,this,&MainWindow::aliyunok);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::aliyunok()
+{
+   qDebug()<<"MQTT:aliyun";
+
+
+}
+void MainWindow::mqsub(const QMQTT::Message& message)
+{
+    qDebug()<<"MQTT:"<<message.topic()<<":"<<message.payload();
+
+}
+void MainWindow::somethingmqsub(const QString& topic, const quint8 qos)
+{
+    qDebug()<<"topic:"<<topic<<"qos"<<qos;
+
+}
+
+
+void MainWindow::on_ButtonMqtt_clicked()
+{
+    static bool flag=true;
+    if(flag){
+        flag=false;
+        client->connectToHost();
+
+        connect(client,&QMQTT::Client::received,this,&MainWindow::mqsub);
+    }else{
+        flag=true;
+        client->disconnectFromHost();
+    }
+}
+
+void MainWindow::on_sendButton_clicked()
+{
+    QMQTT::Message sendmessage;
+    sendmessage.setTopic(ui->topic->text());
+    sendmessage.setPayload(ui->meg->text().toUtf8());
+    sendmessage.setQos(0);
+    client->publish(sendmessage);
+}
+
+void MainWindow::on_sub_topic_editingFinished()
+{
+     client->subscribe(ui->sub_topic->text(),0);
+     qDebug()<<"subscript";
+}
---
+++ mqtt_test/mainwindow.h
@@ -0,0 +1,47 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+//#include "QtMqtt/qmqtt_client.h"
+#include "qmqtt.h"
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+    void mqsub(const QMQTT::Message& message);
+    void somethingmqsub(const QString& topic, const quint8 qos);
+    void on_ButtonMqtt_clicked();
+
+    void on_sendButton_clicked();
+
+    void on_sub_topic_editingFinished();
+    void aliyunok();
+private:
+    Ui::MainWindow *ui;
+//    QHostAddress MQTTIP=QHostAddress("192.168.188.103");
+//    QMQTT::Client *client = new QMQTT::Client(MQTTIP, 1883);
+    // 三元组信息设置
+
+    QString    ProductKey="a1jbawDQyka";//需要跟阿里云Iot平台一致;
+    QString    DeviceName="ESP8266";//需要跟阿里云Iot平台一致;
+    QString    DeviceSecret="P28V2o1MOwnGjW1BuO6qeHsXhWrAfcPw";//需要跟阿里云Iot平台一致;
+    QString    RegionId="cn-shanghai";
+
+    QString    targetServer = "a1jbawDQyka.iot-as-mqtt.cn-shanghai.aliyuncs.com";//域名
+    QString    clientId="ESP8266|securemode=3,signmethod=hmacsha1|";//这里随便写，最好是设备的Mac地址
+    QString     message ="clientId"+clientId+"deviceName"+DeviceName+"productKey"+ProductKey;
+
+    QMQTT::Client *client = new QMQTT::Client(targetServer, 1883,0,false);
+};
+
+#endif // MAINWINDOW_H
